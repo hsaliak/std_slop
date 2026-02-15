d42f35a47b3b43c02906cfcc100d7393e2267f72
hsaliak <hsaliak@gmail.com>
2026-02-15 02:59:46 -0500

    alignment

--- interface/ui.cpp
+++ interface/ui.cpp
@@ -473,7 +473,7 @@ void PrintToolResultMessage([[maybe_unused]] const std::string& name, const std:
   // Print Summary
   std::string summary =
       absl::Substitute("$0 $1 ($2 lines)", is_error ? icons::Error : icons::Success, status, out_lines.size());
-  std::cout << prefix << "    " << Colorize("│", "", ansi::Metadata) << " " << Colorize(summary, "", color)
+  std::cout << prefix << "    " << Colorize("  │", "", ansi::Metadata) << " " << Colorize(summary, "", color)
             << std::endl;
   if (is_error && IsNetworkError(result)) {
     int printed = 0;
